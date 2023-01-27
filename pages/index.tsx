bd395274a31a8ee4d8d880bba2a3522cc25d7abc
ONE Webdesign <info@onewebdesign.nl>
2023-01-27 01:40:51 +0100

    UPDATE: content

--- pages/index.tsx
+++ pages/index.tsx
@@ -25,19 +25,26 @@ export default function Home() {
 
     <Translator />
 
-    <section className='mt-16 text-sm leading-8'>
-      <h1 className='uppercase tracking-wider text-xl'><span className='font-extrabold'>AI powered</span> informal to formal corrections.</h1>
-      <div className='w-8 h-[1px] my-4 bg-zinc-700' />
-      <span className='text-zinc-500'>
+    <section className='mt-16 text-sm leading-8 grid text-zinc-500 gap-8'>
+      <div>
+        <h1 className='uppercase tracking-wider text-xl'>
+          <span className='font-extrabold'>AI powered</span> informal to formal corrections.
+        </h1>
+        <div className='w-8 h-[1px] my-4 bg-zinc-700' />
         Text To Formal provides state of the art AI technology to correct and translate your informal texts to formal texts in 40+ languages.
         Our translator allows companies to engage employees who do not master a language on a professional level, to convert or translate informal texts into formal,
         professional texts ready to be used in B2B and B2C environments. Continuous Machine Learning further improves our language translation
         and correction model on a daily basis, allowing our translator to correct texts to a level that cannot be distinguished from native levels.
-        <br /><br />
-        Text To Formal currently supports translations from and to more than 40 different languages. Currently supported languages are:
+      </div>
+      <div>
+        <h2 className='uppercase tracking-wider text-xl'>
+          <span className='font-extrabold'>Informal to formal</span> translations in 40+ languages.
+        </h2>
+        <div className='w-8 h-[1px] my-4 bg-zinc-700' />
+        Text To Formal currently supports formal text translations and corrections from and to more than 40 different languages. Currently supported languages are:
         <br />
-        <span className='text-sm'>{allLanguages}.</span>
-      </span>
+        <span className='text-xs'>{allLanguages}.</span>
+      </div>
     </section>
   </>
 }
